b042ba64d11bacea723ecf598faaa3beee04e252
UserRoot-Luca <desiro.luca@gmail.com>
2021-09-16 23:15:07 +0200

    1.0.1

--- copyright_resources.php
+++ copyright_resources.php
@@ -1,2 +1,4 @@
-<a href="https://icons8.com/icon/0IYgT8wamUIl/storage">Storage icon by Icons8</a>
-<a href="https://www.w3schools.com/css/css_form.asp">form style</a>
+<?php
+    echo "<a href='https://icons8.com/icon/0IYgT8wamUIl/storage'>Storage icon by Icons8</a>";
+    echo "<a href='https://www.w3schools.com/css/css_form.asp'>form style</a>";
+?>
